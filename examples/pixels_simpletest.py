85d7628b537b1c05487badf1b16dd476e3a963bd
Kattni Rembor <kattni@adafruit.com>
2018-08-01 15:29:14 -0400

    Fix for pylint

--- examples/pixels_simpletest.py
+++ examples/pixels_simpletest.py
@@ -14,10 +14,9 @@ def wheel(pos):
         return (0, 0, 0)
     if pos < 85:
         return (int(pos * 3), int(255 - (pos*3)), 0)
-    elif pos < 170:
+    if pos < 170:
         pos -= 85
         return (int(255 - pos*3), 0, int(pos*3))
-    #else:
     pos -= 170
     return (0, int(pos*3), int(255 - pos*3))
 
