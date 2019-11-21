b9d77f16dfae94d7a93802b63251953352227927
Kattni Rembor <kattni@adafruit.com>
2019-11-21 09:54:41 -0500

    Adding sound_level, loud_sound to bluefruit

--- adafruit_circuitplayground/bluefruit.py
+++ adafruit_circuitplayground/bluefruit.py
@@ -44,6 +44,7 @@ import audiocore
 import digitalio
 import board
 import audiopwmio
+import audiobusio
 from adafruit_circuitplayground.circuit_playground_base import CircuitPlaygroundBase
 
 
@@ -65,6 +66,11 @@ class Bluefruit(CircuitPlaygroundBase):
         self._sample = None
         self._sine_wave = None
         self._sine_wave_sample = None
+
+        # Define mic/sound sensor:
+        self._mic = audiobusio.PDMIn(board.MICROPHONE_CLOCK, board.MICROPHONE_DATA,
+                                     sample_rate=16000, bit_depth=16)
+        self._samples = None
 
         super().__init__()
 
@@ -186,6 +192,76 @@ class Bluefruit(CircuitPlaygroundBase):
             while audio.playing:
                 pass
         self._speaker_enable.value = False
+
+    @staticmethod
+    def _normalized_rms(values):
+        mean_values = int(sum(values) / len(values))
+        return math.sqrt(sum(float(sample - mean_values) * (sample - mean_values)
+                             for sample in values) / len(values))
+
+    @property
+    def sound_level(self):
+        """Obtain the sound level from the microphone (sound sensor).
+
+        .. image :: ../docs/_static/microphone.jpg
+          :alt: Microphone (sound sensor)
+
+        This example prints the sound levels. Try clapping or blowing on
+        the microphone to see the levels change.
+
+        .. code-block:: python
+
+          from adafruit_circuitplayground.bluefruit import cpb
+
+          while True:
+              print(cpb.sound_level)
+        """
+        if self._sample is None:
+            self._samples = array.array('H', [0] * 160)
+        self._mic.record(self._samples, len(self._samples))
+        return self._normalized_rms(self._samples)
+
+    def loud_sound(self, sound_threshold=200):
+        """Utilise a loud sound as an input.
+
+        :param int sound_threshold: Threshold sound level must exceed to return true (Default: 200)
+
+        .. image :: ../docs/_static/microphone.jpg
+          :alt: Microphone (sound sensor)
+
+        This example turns the LEDs red each time you make a loud sound.
+        Try clapping or blowing onto the microphone to trigger it.
+
+        .. code-block:: python
+
+          from adafruit_circuitplayground.bluefruit import cpb
+
+          while True:
+              if cpb.loud_sound():
+                  cpb.pixels.fill((50, 0, 0))
+              else:
+                  cpb.pixels.fill(0)
+
+        You may find that the code is not responding how you would like.
+        If this is the case, you can change the loud sound threshold to
+        make it more or less responsive. Setting it to a higher number
+        means it will take a louder sound to trigger. Setting it to a
+        lower number will take a quieter sound to trigger. The following
+        example shows the threshold being set to a higher number than
+        the default.
+
+        .. code-block:: python
+
+          from adafruit_circuitplayground.bluefruit import cpb
+
+          while True:
+              if cpb.loud_sound(sound_threshold=300):
+                  cpb.pixels.fill((50, 0, 0))
+              else:
+                  cpb.pixels.fill(0)
+        """
+
+        return self.sound_level > sound_threshold
 
 
 cpb = Bluefruit()  # pylint: disable=invalid-name
