d2ab3a3dcd49b3bf83243de54a9aad0f0161c9e9
FoamyGuy <foamyguy@gmail.com>
2020-06-16 18:23:34 -0500

    adding play_mp3 example

---
+++ examples/circuitplayground_bluefruit_play_mp3.py
@@ -0,0 +1,12 @@
+"""
+This example plays mp3 audio files from the built-in speaker when the A or B buttons are pressed.
+
+NOTE: This example does NOT support Circuit Playground Express.
+"""
+from adafruit_circuitplayground import cp
+
+while True:
+    if cp.button_a:
+        cp.play_mp3("dip.mp3")
+    if cp.button_b:
+        cp.play_mp3("rise.mp3")
