64869b519a86589843fe4562af36227cd2103160
Alec Delaney <tekktrik@gmail.com>
2022-01-31 21:19:21 -0500

    Update touch all example to use new IterableInput features

--- examples/circuitplayground_touch_all.py
+++ examples/circuitplayground_touch_all.py
@@ -4,18 +4,9 @@
 """This example prints to the serial console when you touch the capacitive touch pads."""
 from adafruit_circuitplayground import cp
 
+print("Here are all the possible touchpads:")
+print(cp.touchpads)
+
 while True:
-    if cp.touch_A1:
-        print("Touched pad A1")
-    if cp.touch_A2:
-        print("Touched pad A2")
-    if cp.touch_A3:
-        print("Touched pad A3")
-    if cp.touch_A4:
-        print("Touched pad A4")
-    if cp.touch_A5:
-        print("Touched pad A5")
-    if cp.touch_A6:
-        print("Touched pad A6")
-    if cp.touch_TX:
-        print("Touched pad TX")
+    print("Touchpads currently registering a touch:")
+    print(cp.touched)
