52fcb6b00797673d93f4b3885c064cdb87dd5931
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:13 -0400

    Update version string

--- adafruit_circuitplayground/bluefruit.py
+++ adafruit_circuitplayground/bluefruit.py
@@ -33,7 +33,7 @@ import audiobusio
 from adafruit_circuitplayground.circuit_playground_base import CircuitPlaygroundBase
 
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_CircuitPlayground.git"
 
 
