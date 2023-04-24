032006887e78c661ae8d80d12526feece0c69f46
Chris Pappalardo <cpappalardo@alvarezandmarsal.com>
2023-04-24 16:06:33 -0700

    added default cpb object that throws import exception when used on an unsupported platform

--- adafruit_circuitplayground/__init__.py
+++ adafruit_circuitplayground/__init__.py
@@ -6,7 +6,17 @@
 
 import sys
 
+
+class PlatformNotSupported:
+    """throws exception when used"""
+
+    def __getattribute__(self, *args, **kwargs):
+        raise ImportError(f"{sys.platform} not supported by this lib")
+
+
 if sys.platform == "nRF52840":
     from .bluefruit import cpb as cp
 elif sys.platform == "Atmel SAMD21":
     from .express import cpx as cp
+else:
+    cp = PlatformNotSupported()
